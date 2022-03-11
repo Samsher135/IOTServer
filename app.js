e0cc1058d2016fad7558783fad30d4a5713cf11d
Samsher135 <12345samsingh.samu@gmail.com>
2022-03-11 19:00:22 +0530

    majorss

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const hbs = require('hbs');
+const event = require('events');
+const EventEmitter = require('stream');
 require('dotenv').config();
+
+let eventEmitter = new EventEmitter();
 
 var app = express();
 
@@ -10,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 let pin_status;
 let state = 1;
+let temp, hum;
 
 app.get('/', (req, res)=>{
     res.render('home');
@@ -27,7 +32,24 @@ app.get('/update_pin', (req, res)=>{
     }
 })
 
-//get_pin
+//read sensor data for client side
+app.get('/get_readings', (req, res)=>{
+    res.status(200).send({temp:temp, hum:hum});
+});
+
+//set temperature and humidity sensor data
+app.get('/readings', (req, res)=>{
+    temp = req.query.temp;
+    hum = req.query.hum;
+    console.log(temp, hum);
+    res.status(200).send("new_readings");
+})
+
+// render sensordata page
+app.get('/sensordata', (req, res)=>{
+    res.render('sensordata');
+});
+
 app.get('/get_pin', (req, res)=>{
     res.status(200).send({pin_status:pin_status});
 })
